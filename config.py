7398d8e241a80bfd5d1ba5fb427d4a40c4067ae8
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-11 18:52:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.getenv("API_HASH", "d6186691704bd901bdab275ceaab88f3")
 # Bot token from @BotFather
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8525366225:AAG_nUH9HfmDz8TpBxg-gt2pMDesnfY71L4")
 
-START_IMAGE = "https://example.com/start.jpg"  # image URL or file_id
+START_IMAGE = "https://telegram.me/share/url?url=https://files.catbox.moe/zttfbe.jpg"  # image URL or file_id
 
 FORCE_JOIN_CHANNEL = "TechyNetwork"  # without @
 

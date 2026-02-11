566ca5daef0dec901eefe8917f40ffc443060920
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-11 18:38:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
+bot = Client(
+    "nexa_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+# Handlers load
+import handlers.start
+import handlers.connect
+import handlers.otp
+
+if __name__ == "__main__":
+    print("🚀 Bot Running...")
+    bot.run()

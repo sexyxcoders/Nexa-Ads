d9eee7f8a85b8a0e8d5eeaf6bbae366e53a43a3c
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-11 19:43:33 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,15 @@
-# Ruk
+# handlers/__init__.py
+
+"""
+This file initializes all handlers for the bot.
+Importing this module will automatically register all handlers.
+"""
+
+# Import all your handler modules
+from . import start
+from . import connect
+from . import otp
+
+# You can add more handlers here as you create them
+# from . import broadcast
+# from . import admin

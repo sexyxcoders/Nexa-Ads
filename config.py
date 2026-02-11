ed839f73d8f74f7d06dff8bc94802fba07285db6
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-11 18:49:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 # Telegram API credentials (from my.telegram.org)
-API_ID = int(os.getenv("API_ID", "123456"))  # replace if not using env
-API_HASH = os.getenv("API_HASH", "your_api_hash_here")
+API_ID = int(os.getenv("API_ID", "22657083"))  # replace if not using env
+API_HASH = os.getenv("API_HASH", "d6186691704bd901bdab275ceaab88f3")
 
 # Bot token from @BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8525366225:AAG_nUH9HfmDz8TpBxg-gt2pMDesnfY71L4")
 
 START_IMAGE = "https://example.com/start.jpg"  # image URL or file_id
 
-FORCE_JOIN_CHANNEL = "YourChannelUsername"  # without @
+FORCE_JOIN_CHANNEL = "TechyNetwork"  # without @
 
 # Folder where user session files will be stored
 SESSION_FOLDER = "sessions/"
